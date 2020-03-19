bd4a9b652e2de3d400c67783b41b0a92bd6006fa
Johannes Buchner <johannes.buchner.acad@gmx.com>
2020-03-19 23:26:04 +0100

    normalise better; beautifying

--- ratio.py
+++ ratio.py
@@ -60,14 +60,16 @@ pop = pandas.concat([
 
 beds = pandas.read_csv('UNdata_Export_hospitalbeds.csv')
 
+marked_countries = ['US', 'Germany', 'Japan', 'Italy', 'Korea, South', 'Iran']
+
 #print("countries:", sorted(set(pop["Country or Area"])))
 
-a = plt.figure(figsize=(7, 8))
+a = plt.figure(figsize=(6, 6))
 ax = a.gca()
-b = plt.figure(figsize=(7, 8))
+b = plt.figure(figsize=(6, 6))
 bx = b.gca()
 
-min_dead = 6
+#min_dead = 4
 
 print("per-country extraction:")
 print()
@@ -78,21 +80,21 @@ for (i, row1), (_, row2), (_, row3) in zip(d1.iterrows(), d2.iterrows(), d3.iter
 	timeseries_dead = np.array(row2[4:], dtype=int)
 	timeseries_recovered = np.array(row3[4:], dtype=int)
 	country = row1.name
-	if timeseries_dead.max() < 1:
-		#print("skipping", country, "not enough dead", timeseries_dead.max())
-		continue
 	pop_country = pop[pop["Country or Area"] == country_name_replacement.get(row1.name, row1.name)]
 	if len(pop_country) == 0: # or pop_country.Year.dtype.char == 'O':
 		print("skipping", country, "no population data")
 		continue
 	beds_country = beds[beds["Country or Area"] == country_name_replacement.get(row1.name, row1.name)]
 	year = pandas.to_numeric(beds_country["Year(s)"])
-	beds_recent = np.array(beds_country.Value[year == year.max()])
+	# from "number of beds per 10000 people" compute "number of beds per person":
+	beds_recent = np.array(beds_country.Value[year == year.max()]) / 10000
 	if len(beds_recent) == 0:
 		beds_recent = 0
+		#print("skipping", country, "no bed data")
+		#continue
 	else:
 		beds_recent = beds_recent[0]
-	
+
 	#if country not in ['US', 'Germany', 'Japan', 'Italy', 'Korea, South']:
 	#	continue
 	
@@ -102,99 +104,118 @@ for (i, row1), (_, row2), (_, row3) in zip(d1.iterrows(), d2.iterrows(), d3.iter
 		for ages in agegroups]
 	vulnerable_number = np.array(num_people) * agegroups_mortality
 	
-	print("%20s %4d %6d %14d %.2f%%" % (country, beds_recent, sum(timeseries_dead), 
+	print("%20s %6d %6d %14d %.2f%%" % (country, beds_recent, max(timeseries_dead), 
 		sum(num_people), sum(vulnerable_number) * 100 / sum(num_people)))
+	
+	# for small countries, require a higher number of dead, 
+	# otherwise the estimates are all over the place
+	min_dead = max(4,  1 * 2 * 100000 / sum(vulnerable_number))
+	
+	if timeseries_dead.max() < min_dead:
+		print("skipping", country, "not enough dead", timeseries_dead.max(), 1 * 2 * 100000 / sum(vulnerable_number))
+		continue
 	
 	timeseries_cases = timeseries_reported - timeseries_recovered
 	
 	mask = timeseries_dead >= min_dead
+	if not mask.any():
+		print("skipping", country, "not enough dead", timeseries_dead.max())
+	
 	#mask = np.logical_and(timeseries_dead >= 3, timeseries_reported / vulnerable_number.sum() > 1e-5)
 	x = timeseries_cases[mask] / vulnerable_number.sum()
 	y = timeseries_dead[mask] / timeseries_reported[mask]
 	if mask.any():
-		l, = ax.plot(x, y,
-			'o-' if country in ['US', 'Germany', 'Japan', 'Italy', 'Korea, South'] else 's-', ms=2,
-			label=country, alpha=0.5)
+		l, = ax.plot(x[-1], y[-1],
+			'o' if country in marked_countries else 's')
+		ax.text(x[-1], y[-1], country, va='bottom', ha='left', size=8, rotation=33)
+		if mask.sum() > 10:
+			ax.plot(x, y,
+				'o-' if country in marked_countries else 's-', ms=2,
+				label=country, alpha=0.5, color=l.get_color())
 	
-	if len(y) > 0:
-		ax.plot(x[-1], y[-1],
-			'o' if country in ['US', 'Germany', 'Japan', 'Italy', 'Korea, South'] else 's',
-			color=l.get_color())
-		ax.text(x[-1], y[-1], country, va='bottom', ha='left', size=8)
 	elif timeseries_cases[-1] / vulnerable_number.sum() > 1e-5:
 		ax.plot(timeseries_cases[-1] / vulnerable_number.sum(), 1e-3,
-			'o' if country in ['US', 'Germany', 'Japan', 'Italy', 'Korea, South'] else 's')
+			'o' if country in marked_countries else 's')
 		ax.text(timeseries_cases[-1] / vulnerable_number.sum(), 1e-3,
 			'  ' + country, va='bottom', ha='center', size=6, rotation=90)
 
 	x = timeseries_cases[mask] / vulnerable_number.sum() / beds_recent
 	y = timeseries_dead[mask] / timeseries_reported[mask]
 	if mask.any():
-		l, = bx.plot(x, y,
-			'o-' if country in ['US', 'Germany', 'Japan', 'Italy', 'Korea, South'] else 's-', ms=2,
-			label=country, alpha=0.5)
-	
-	if len(y) > 0:
-		bx.plot(x[-1], y[-1],
-			'o' if country in ['US', 'Germany', 'Japan', 'Italy', 'Korea, South'] else 's',
-			color=l.get_color())
+		l, = bx.plot(x[-1], y[-1],
+			'o' if country in marked_countries else 's',
+			)
 		bx.text(x[-1], y[-1], country, va='bottom', ha='left', size=8, rotation=33)
-	elif timeseries_cases[-1] / vulnerable_number.sum() / beds_recent > 1e-7:
+		if mask.sum() > 10:
+			bx.plot(x, y,
+				'o-' if country in marked_countries else 's-', ms=2,
+				label=country, alpha=0.5, color=l.get_color())
+	
+	elif timeseries_cases[-1] / vulnerable_number.sum() / beds_recent > 1e-3 or True:
 		bx.plot(timeseries_cases[-1] / vulnerable_number.sum() / beds_recent, 1e-3,
-			'o' if country in ['US', 'Germany', 'Japan', 'Italy', 'Korea, South'] else 's')
+			'o' if country in marked_countries else 's')
 		bx.text(timeseries_cases[-1] / vulnerable_number.sum() / beds_recent, 1e-3,
 			'  ' + country, va='bottom', ha='center', size=6, rotation=90)
 
 plt.sca(ax)
-ax.text(0.02, 0.98, """How to read this graph:
+ax.text(0.02, 1.0, """How to read this graph:
 The horizontal axis represents penetration of the population.
 The vertical axis indicates under-reporting of infections
 (compare to Diamond Princess and South Korea).
 Deaths (rare!) occur after a delay. For example, in China and 
 South Korea, deaths keep increasing after no new infections.
-
-Definitions:
-Vulnerable population = Country demographics age groups multiplied 
-by their relative mortality (taken from Riou et al. 2020).
-# of infected = confirmed cases - recovered cases
-""", va='top', ha='left', transform=ax.transAxes)
-plt.xlabel('# of Infected / Vulnerable Population')
-plt.ylabel('# of Deaths / # of Confirmed cases')
-plt.xscale('log')
-plt.yscale('log')
-plt.hlines(7/712, 1e-5, 2e-2, linestyles=['--'], color='gray')
-plt.text(2e-2, 7/712, 'Diamond Princess', ha='right', va='bottom', size=6)
-plt.ylim(1e-3, 1)
-plt.xlim(1e-5, 2e-2)
-#plt.legend(loc='best', ncol=3, prop=dict(size=8))
-plt.savefig('ratio.pdf', bbox_inches='tight')
-plt.savefig('ratio.png', bbox_inches='tight')
-plt.close()
-
-#plt.plot([1e-6, 1e-3], [5e-6/1e-6, 5e-6/1e-3], ':', color='gray', alpha=0.5)
-
-plt.sca(bx)
-bx.text(0.02, 0.98, """How to read this graph:
-The horizontal axis represents penetration of the population.
-The vertical axis indicates under-reporting of infections
-(compare to Diamond Princess and South Korea).
-Deaths (rare!) occur after a delay. For example, in China and 
-South Korea, deaths keep increasing after no new infections.
+The horizontal axis represents stress on the health care system.
 
 Definitions:
 Vulnerable population = Country demographic age groups multiplied 
 by their relative mortality (taken from Riou et al. 2020).
 # of infected = confirmed cases - recovered cases
-""", va='top', ha='left', transform=bx.transAxes)
-plt.xlabel('# of Infected / Vulnerable Population / # hospital beds per 10,000 people')
+""", va='bottom', ha='left', transform=ax.transAxes, size=10)
+
+plt.xlabel('# of Infected / Vulnerable Population')
 plt.ylabel('# of Deaths / # of Confirmed cases')
 plt.xscale('log')
 plt.yscale('log')
-plt.hlines(7/712, 1e-7, 1e-3, linestyles=['--'], color='gray')
-plt.text(1e-3, 7/712, 'Diamond Princess', ha='right', va='bottom', size=6)
-plt.ylim(1e-3, 1)
-plt.xlim(1e-7, 1e-3)
+plt.xticks([1e-4, 1e-3, 1e-2], ['0.01%', '0.1%', '1%'])
+plt.yticks([1e-3, 1e-2, 1e-1, 1], ['0.1%', '1%', '10%', '100%'])
+plt.hlines(7/712, 1e-5, 2e-2, linestyles=['--'], color='gray')
+plt.text(2e-2, 7/712, 'Diamond Princess', ha='right', va='bottom', size=6)
+plt.ylim(1e-3, 0.2)
+plt.xlim(1e-5, 2e-2)
+#plt.legend(loc='best', ncol=3, prop=dict(size=8))
+plt.savefig('ratio.pdf', bbox_inches='tight')
+plt.savefig('ratio.png', bbox_inches='tight', dpi=120)
+plt.close()
+
+#plt.plot([1e-6, 1e-3], [5e-6/1e-6, 5e-6/1e-3], ':', color='gray', alpha=0.5)
+
+plt.sca(bx)
+bx.text(0.98, 0.98, "@JohannesBuchner (non-expert)", va='top', ha='right',
+	transform=bx.transAxes, size=8, color="gray")
+
+bx.text(0.02, 1.0, """How to read this graph:
+The vertical axis indicates under-reporting of infections
+(compare to Diamond Princess and South Korea).
+Deaths (rare!) occur after a delay. For example, in China and 
+South Korea, deaths keep increasing after no new infections.
+The horizontal axis represents stress on the health care system.
+
+Definitions:
+Vulnerable population = Country demographic age groups multiplied 
+by their relative mortality (taken from Riou et al. 2020).
+# of infected = confirmed cases - recovered cases
+""", va='bottom', ha='left', transform=bx.transAxes, size=10)
+plt.xlabel("# of Infected / Vulnerable Population / # hospital beds per person\n\nHealth care system stress $\\rightarrow$")
+plt.ylabel('reported mortality $\\rightarrow$\n\n# of Deaths / # of Confirmed cases')
+plt.xscale('log')
+plt.yscale('log')
+plt.xticks([1e-2, 1e-1, 1], ['1%', '10%', '100%'])
+plt.yticks([1e-3, 1e-2, 1e-1, 1], ['0.1%', '1%', '10%', '100%'])
+plt.hlines(7/712, 1e-3, 8, linestyles=['--'], color='gray')
+plt.text(8, 7/712, 'Diamond Princess', ha='right', va='bottom', size=6)
+plt.ylim(1e-3, 0.2)
+plt.xlim(1e-3, 8)
 #plt.legend(loc='best', ncol=3, prop=dict(size=8))
 plt.savefig('ratio_beds.pdf', bbox_inches='tight')
-plt.savefig('ratio_beds.png', bbox_inches='tight')
+plt.savefig('ratio_beds.png', bbox_inches='tight', dpi=120)
 plt.close()
